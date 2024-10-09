2d050accca0d9b00a6bad6538e8f59a5deb196db
Anaïs <a.aerts@interface3.be>
2024-10-09 09:58:59 +0200

    Add fixture for User

---
+++ src/DataFixtures/ProfilFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory as FakerFactory;
+
+class ProfilFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+    public function load(ObjectManager $manager): void
+    {
+        $faker = FakerFactory::create('fr_FR');
+
+        for ($i=0; $i < 10; $i++) { 
+            $user = new User();
+            
+            $user->setEmail($faker->email);
+            $user->setPassword($this->passwordHasher->hashPassword($user,'password123'));
+            $user->setNom($faker->lastName);
+            $user->setPrenom($faker->firstName);
+            $user->setPseudo($faker->userName);
+            $user->setAvatar($faker->imageUrl(150, 150, 'people', true));
+
+            $user->setRoles(['ROLE_USER']);
+
+            $manager->persist($user);
+        }
+
+
+        $manager->flush();
+    }
+}
