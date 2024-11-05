0d8124636e03bd5311a358a83dbca2f5af255e50
Anaïs <a.aerts@interface3.be>
2024-11-05 11:01:12 +0100

    Poster question fonctionnel

--- src/Controller/EntraideController.php
+++ src/Controller/EntraideController.php
@@ -2,20 +2,27 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\User;
 use App\Entity\Question;
+use App\Form\QuestionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EntraideController extends AbstractController
 {
     private $doctrine;
+    private $slugger;
 
-    public function __construct(ManagerRegistry $doctrine)
+    public function __construct(ManagerRegistry $doctrine, SluggerInterface $slugger)
     {
         $this->doctrine = $doctrine;
+        $this->slugger = $slugger;
     }
 
     #[Route('/entraide', name: 'entraide')]
@@ -36,9 +43,65 @@ class EntraideController extends AbstractController
     }
 
     #[Route('/entraide/poster', name: 'entraide_ajouter')]
-    public function ajouter(): Response
+    public function ajouter(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('entraide/entraide_ajouter.html.twig');
+        // Créer une nouvelle question
+        $question = new Question();
+
+        // Créer le formulaire
+        $form = $this->createForm(QuestionType::class, $question);
+
+        // Traiter la requête
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $slug = $this->slugger->slug($question->getTitre())->lower();
+            $question->setSlug($slug);
+        
+            // Vérifiez que l'utilisateur est connecté
+            $user = $this->getUser();
+            if (!$user) {
+                $this->addFlash('error', 'Vous devez être connecté pour poser une question.');
+                return $this->redirectToRoute('connexion'); // Redirigez vers la page de connexion
+            }
+
+            // Associez l'utilisateur connecté
+            $question->setUser($user);
+
+            // Définir la date de publication
+            $question->setDatePublication(new DateTime());
+        
+            // Gestion de l'image
+            $imageFile = $form->get('image')->getData();
+            if ($imageFile) {
+                // Après avoir persisté l'entité, on obtient l'ID de la question
+                $entityManager->persist($question);
+                $entityManager->flush();
+        
+                // Maintenant que l'ID est généré, créez le dossier de l'utilisateur
+                $questionID = $question->getId();
+                $questionDir = $this->getParameter('questions_directory') . '/' . $questionID;
+                mkdir($questionDir, 0755, true);
+        
+                // Déplacer le fichier image dans le sous-dossier de la question
+                $newFileName = uniqid() . '.' . $imageFile->guessExtension();
+                $imageFile->move($questionDir, $newFileName);
+        
+                // Enregistrez le nom de fichier dans la base de données
+                $question->setImage($newFileName);
+            }
+        
+            // Enregistrer la question après avoir défini l'image
+            $entityManager->persist($question);
+            $entityManager->flush();
+        
+            return $this->redirectToRoute('entraide');
+        }
+        
+
+        return $this->render('entraide/entraide_ajouter.html.twig', [
+            'form' => $form,
+        ]);
     }
     
     #[Route('/entraide/d/{user_slug}/{question_slug}', name: 'entraide_detail')]
---
+++ src/Form/QuestionType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\Question;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class QuestionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('contenu')
+            ->add('image', FileType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/jpg',
+                            'image/png',
+                        ]
+                    ])
+                ]
+            ])
+            // ->add('slug')
+            // ->add('datePublication', null, [
+            //     'widget' => 'single_text',
+            // ])
+            // ->add('user', EntityType::class, [
+            //     'class' => User::class,
+            //     'choice_label' => 'id',
+            // ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Question::class,
+        ]);
+    }
+}
