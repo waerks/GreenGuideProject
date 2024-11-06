841bbf07987c596cd823d26f7e6efd370c94baae
Anaïs <a.aerts@interface3.be>
2024-11-06 15:33:05 +0100

    affichage recette sur profil ok

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Recette;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfilController extends AbstractController
 {
+    private $doctrine;
+    private $slugger;
+
+    public function __construct(ManagerRegistry $doctrine, SluggerInterface $slugger)
+    {
+        $this->doctrine = $doctrine;
+        $this->slugger = $slugger;
+    }
+
     #[Route('/profil', name: 'profil')]
     public function index(): Response
     {
+        // Récupérer l'utilisateur connecté
+        $user = $this->getUser();
+
+        // Récupérer les recettes de cet utilisateur
+        $recettes = $this->doctrine->getRepository(Recette::class)->findBy(['user' => $user]);
+
         return $this->render('profil/profil.html.twig', [
-            'controller_name' => 'ProfilController',
+            'recettes' => $recettes,
         ]);
     }
 }
